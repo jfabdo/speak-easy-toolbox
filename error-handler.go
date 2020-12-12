547e0950b96b9bbd5ea6c1b75e8fae23b9c4b007
Jacqueline Abdo <jfabdo@gmail.com>
2020-12-12 06:28:19 -0700

    Handled redundant library error

--- error-handler.go
+++ error-handler.go
@@ -1,12 +1,11 @@
 package toolbox
 
 import (
-	"evilrus.c/speak-easy/toolbox/toolbox"
 	"github.com/mediocregopher/radix/v3"
 )
 
 //ErrorHandler pushes an error to the error q
 func ErrorHandler(err error) {
-	clnt := toolbox.GetClient()
+	clnt := GetClient()
 	clnt.Do(radix.Cmd(nil, "rpush", "error", string(err.Error())))
 }
