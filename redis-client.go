a44334389caf029e891672ce23e042543d2079c0
Jacqueline Abdo <jfabdo@gmail.com>
2020-12-12 14:48:06 -0700

    tweaking

--- redis-client.go
+++ redis-client.go
@@ -3,7 +3,6 @@ package toolbox
 import (
 	"log"
 	"os"
-	"time"
 
 	"github.com/mediocregopher/radix/v3"
 )
@@ -19,13 +18,26 @@ func GetPool() *radix.Pool {
 	}
 }
 
-//GetPubSub returns something to listen to pubsub with
-func GetPubSub(channel string) chan radix.PubSubMessage {
+//GetPubSubConn returns only the working connection for redis
+func GetPubSubConn() radix.Conn {
 	conn, err := radix.Dial("tcp", os.Getenv("ERU_SE_REDIS_IP"))
 	if err != nil {
-		println(err)
-		panic(err)
+		//stuff
 	}
+	return conn
+}
+
+//Publish publishes a message to a certain
+func Publish(channel string, body string) {
+	conn := GetPubSubConn()
+	ps := radix.PubSub(conn)
+	defer ps.Close()
+	conn.Do(radix.Cmd("publish", channel, body))
+}
+
+//GetSub returns something to listen to pubsub with
+func GetSub(channel string) chan radix.PubSubMessage {
+	conn := GetPubSubConn()
 	ps := radix.PubSub(conn)
 	defer ps.Close()
 
@@ -41,16 +53,6 @@ func GetPubSub(channel string) chan radix.PubSubMessage {
 //WaitForPubSub will wait when invoked
 func WaitForPubSub(msgCh chan radix.PubSubMessage) radix.PubSubMessage {
 	errCh := make(chan error, 1)
-	go func() {
-		ticker := time.NewTicker(5 * time.Second)
-		defer ticker.Stop()
-		// for range ticker.C {
-		// 	if err := ps.Ping(); err != nil {
-		// 		errCh <- err
-		// 		return
-		// 	}
-		// }
-	}()
 
 	for {
 		select {
